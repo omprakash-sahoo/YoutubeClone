2f33ad2c096f35bd036a86c8d7732a7aa876fa2e
Om prakash <omprakashsahoo.bdk@gmail.com>
2025-08-07 01:45:01 +0530

    fixed the sidebar close issue on redirection

--- src/Component/VideoContainer.js
+++ src/Component/VideoContainer.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
 import { YOUTUBE_VIDEO_API } from "../utils/constant";
 import { Link } from "react-router";
+import { openMenu } from "../utils/appSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const VideoContainer = () => {
   const [video, setVideo] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getVideos();
+    dispatch(openMenu());
   }, []);
 
   const getVideos = async () => {
