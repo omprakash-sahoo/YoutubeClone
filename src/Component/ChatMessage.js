582e0d2cb6dbf3e6265de1e6706a918e358b03e3
Om prakash <omprakashsahoo.bdk@gmail.com>
2025-08-07 00:12:21 +0530

    Search bar, watch page, routing

---
+++ src/Component/ChatMessage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ChatMessage = ({ name, message }) => {
+  return (
+    <div className="ml-2 border border-slate-400 h-[500px] w-[400px] rounded-2xl text-sm ">
+      <div className="flex text-center mt-2 ml-2 p-2 hover:bg-gray-100 hover:rounded-2xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+        <span className="font-semibold mx-2 text-gray-500">{name}</span>
+        <span>{message}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ChatMessage;
---
+++ src/Component/LiveChat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatMessage from "./ChatMessage";
+
+const LiveChat = () => {
+  return (
+    <div>
+      <ChatMessage name={"Om Prakash"} message={"Hello youtube"} />
+    </div>
+  );
+};
+
+export default LiveChat;
