c8ab6a99f2df772ac9d5823940ede27d9a52ec4d
Om prakash <omprakashsahoo.bdk@gmail.com>
2025-08-07 01:15:02 +0530

    Live chat feature

--- src/Component/LiveChat.js
+++ src/Component/LiveChat.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateName, generateRandomText } from "../utils/helper";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      dispatch(
+        addMessage({
+          name: generateName(),
+          message: generateRandomText(10),
+        })
+      );
+    }, 1500);
+    return () => clearInterval(i);
+  }, []);
+  const chatmessage = useSelector((store) => {
+    return store.chat.message;
+  });
   return (
-    <div>
-      <ChatMessage name={"Om Prakash"} message={"Hello youtube"} />
+    <div className="ml-2 border border-slate-400 h-[500px] w-[400px] rounded-2xl text-sm overflow-auto">
+      {chatmessage.map((chat, index) => {
+        return (
+          <ChatMessage key={index} name={chat.name} message={chat.message} />
+        );
+      })}
     </div>
   );
 };
